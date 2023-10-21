2cce99aa916ff265848c3e5472430b32e7031d17
OneLiL05 <kirillofficial0507@gmail.com>
2023-10-21 23:38:36 +0300

    feat: add button for toggling theme

--- src/core/services/axios.service.ts
+++ src/core/services/axios.service.ts
@@ -1,15 +1,7 @@
 import axios from "axios";
-import { LOCAL_KEYS } from "core/constants";
-
-const localTokens = localStorage.getItem(LOCAL_KEYS.AUTH_TOKENS);
-
-const accessToken = localTokens ? JSON.parse(localTokens).access_token : null;
 
 const axiosClient = axios.create({
     baseURL: "https://nure-dev.pp.ua/api",
-    headers: {
-        Authorization: `Bearer ${accessToken}`,
-    },
 });
 
 export default axiosClient;
