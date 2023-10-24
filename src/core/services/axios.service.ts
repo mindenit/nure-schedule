5985cd0ab6577496ac7602576832cf346121a4a5
OneLiL05 <kirillofficial0507@gmail.com>
2023-10-24 23:52:06 +0300

    fix: fix issue with calendar's rendering speed

--- src/core/services/axios.service.ts
+++ src/core/services/axios.service.ts
@@ -4,4 +4,17 @@ const axiosClient = axios.create({
     baseURL: "https://nure-dev.pp.ua/api",
 });
 
+axiosClient.interceptors.response.use(
+    function (response) {
+        return response;
+    },
+    function (error) {
+        const res = error.response;
+        if (res.status == 401) {
+            window.location.href = "/signin";
+        }
+        return Promise.reject(error);
+    }
+);
+
 export default axiosClient;
--- src/libs/calendar/core/utils/date.ts
+++
@@ -1,7 +0,0 @@
-export const getCurrentDate = () => {
-  const date = new Date(),
-  year = date.getFullYear(),
-	month = date.getMonth();
-
-  return { date, month, year }
-}
