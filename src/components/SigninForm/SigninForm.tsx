ef4373c00fa620215764056c9c71a70262a223b8
OneLiL05 <kirillofficial0507@gmail.com>
2023-10-25 16:14:15 +0300

    fix: fix typo

--- src/components/SigninForm/SigninForm.tsx
+++ src/components/SigninForm/SigninForm.tsx
@@ -49,7 +49,7 @@ export const SigninForm = () => {
             </Button>
             <S.StyledTextContainer>
                 <C.TitleMedium>
-                    Не має аккаунту? <Link to="/signup">Зареєструватися</Link>
+                    Немає аккаунту? <Link to="/signup">Зареєструватися</Link>
                 </C.TitleMedium>
                 <Link to="/">Продовжити як гість</Link>
             </S.StyledTextContainer>
